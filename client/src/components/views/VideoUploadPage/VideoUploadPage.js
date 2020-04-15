332a642289717b258c95ac04c10e62e93faf7d83
kyomin <kim031504@naver.com>
2020-04-15 12:28:40 +0900

    비디오 업로드 FORM 화면의 Title, Description, Access, Category 부분 작업 완료

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import DropZone from 'react-dropzone';
 
 const { TextArea } = Input;
 const { Title } = Typography;
+
+/*
+    select 태그의 option은 여러 개이므로 이를 하드 코딩하기 보다는
+    map 함수를 통해서 그리기 위해 미리 배열로 정의하는 것이다.
+*/
+const accessOptions = [
+    {value: 0, label: "Private"},
+    {value: 1, label: "Public"},
+];
+
+const categoryOptions = [
+    {value: 0, label: "Film & Animation"},
+    {value: 1, label: "Autos & Vehicles"},
+    {value: 2, label: "Music"},
+    {value: 3, label: "Pets & Animals"}
+];
+
 function VideoUploadPage() {
+    // 함수 기반 컴포넌트에서의 state 변수 관리!
+    const [videoTitle, setVideoTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [access, setAccess] = useState(0);  // 기본 값은 0으로 설정! (private : 0, public : 1)
+    const [category, setCategory] = useState("Film & Animation");
+
+    const onTitleChange = (e) => {
+        setVideoTitle(e.currentTarget.value);
+    };
+
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value);
+    };
+
+    const onAccessChange = (e) => {
+        setAccess(e.currentTarget.value);
+    };
+
+    const onCategoryChange = (e) => {
+        setCategory(e.currentTarget.value);
+    };
+
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -37,27 +76,32 @@ function VideoUploadPage() {
                 <br />
                 <label>Title</label>
                 <Input
-                    onChange
-                    value
+                    onChange={onTitleChange}
+                    value={videoTitle}
                 />
                 <br />
                 <br />
                 <label>Description</label>
                 <TextArea
-                    onChange   
-                    value
+                    onChange={onDescriptionChange}
+                    value={description}
                 />
                 <br />
                 <br />
 
-                <select onChange>
-                    <option key value></option>
+                <select onChange={onAccessChange}>
+                    {/* html과 어울러 script(변수 + 함수)를 쓰기 위해서는 {}로 묶어준다. */}
+                    {accessOptions.map( (item, index) => {
+                        return <option key={index} value={item.value}>{item.label}</option>    // key에 값을 넣어줘야 에러 메시지가 안 뜬다.
+                    })}
                 </select>
                 <br />
                 <br />
 
-                <select onChange>
-                    <option key value></option>
+                <select onChange={onCategoryChange}>
+                    {categoryOptions.map( (item, index) => {
+                        return <option key={index} value={item.value}>{item.label}</option>
+                    })}
                 </select>
                 <br />
                 <br />
