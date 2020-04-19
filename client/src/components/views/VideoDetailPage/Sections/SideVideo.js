d07064fb1d96741cbc2e62b6c87a6cdefd2a28e8
kyomin <kim031504@naver.com>
2020-04-19 15:39:47 +0900

    사이드 비디오에 링크 걸기 완료

--- client/src/components/views/VideoDetailPage/Sections/SideVideo.js
+++ client/src/components/views/VideoDetailPage/Sections/SideVideo.js
@@ -25,7 +25,7 @@ function SideVideo() {
             <div key={idx} style={{ display: 'flex', marginBottom: '1rem', padding: '0 2rem' }}>
                 {/* 왼쪽 부분 */}
                 <div style={{ width: '40%', marginRight: '1rem' }}>
-                    <a href>
+                    <a href={`/video/${video._id}`}>
                         <img 
                             style={{ width: '100%', height: '100%' }}
                             src={`${constants.URL_BACK}/${video.thumbnail}`}
@@ -35,7 +35,7 @@ function SideVideo() {
                 </div>
                 {/* 오른쪽 부분 */}
                 <div style={{ width: '50%' }}>
-                    <a href style={{ color: 'gray' }}>
+                    <a href={`/video/${video._id}`} style={{ color: 'gray' }}>
                         <span style={{ fontSize: '1rem', color: 'black' }}> {video.title} </span><br />
                         <span>{video.writer.name}</span><br />
                         <span>{video.views} views</span><br />
